38865776ad14c43d1631e2311ed640949e2e71e3
Torgrim Teigstad <torgrimt@oppetid.no>
2017-01-04 10:53:48 +0100

    first commit

---
+++ garasjeagogo.go
@@ -0,0 +1,33 @@
+package garasjeagogo
+
+import (
+	// "fmt"
+	"time"
+	// MQTT "github.com/eclipse/paho.mqtt.golang"
+	// "github.com/stianeikeland/go-rpio"
+	"github.com/spf13/viper"
+	"log"
+)
+
+
+func setupConfiguration() {
+	viper.SetConfigName("garasjeagogo")
+	viper.AddConfigPath("/etc/garasjeagogo/")
+	viper.AddConfigPath("$HOME/.garasjeagogo/")
+	viper.AddConfigPath("./")
+
+	configError := viper.ReadInConfig()
+	if configError != nil {
+		log.Panicf("Error reading configuration: %v\n", configError)
+	}
+
+}
+
+
+func main() {
+	log.Println("Started garasjeagogo...")
+	setupConfiguration()
+	for {
+		time.Sleep(30 * time.Second)
+	}
+}
